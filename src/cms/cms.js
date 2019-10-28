0c58cf0e5b8cafe69faaedfb18bb5211fb16978b
Mauricio Sánchez <msanchez@shibumi.cl>
2019-10-28 20:16:16 -0300

    .

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -4,15 +4,11 @@ import cloudinary from 'netlify-cms-media-library-cloudinary'
 
 import AboutPagePreview from './preview-templates/AboutPagePreview'
 import BlogPostPreview from './preview-templates/BlogPostPreview'
-import ProductPagePreview from './preview-templates/ProductPagePreview'
 import IndexPagePreview from './preview-templates/IndexPagePreview'
-import ShibumiPagePreview from './preview-templates/ShibumiPagePreview'
 
 CMS.registerMediaLibrary(uploadcare)
 CMS.registerMediaLibrary(cloudinary)
 
 CMS.registerPreviewTemplate('index', IndexPagePreview)
 CMS.registerPreviewTemplate('about', AboutPagePreview)
-CMS.registerPreviewTemplate('products', ProductPagePreview)
 CMS.registerPreviewTemplate('blog', BlogPostPreview)
-CMS.registerPreviewTemplate('shibumi', ShibumiPagePreview)
